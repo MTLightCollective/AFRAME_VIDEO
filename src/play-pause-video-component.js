e52a172acf5a6fc60660d233c8f2789f39a28a89
arthurM <arthur.mougin@live.fr>
2023-12-06 10:37:50 -0500

    changer a-camera en a-entity + camera and stuff Réorganisation de code en scripts séparés amélioration structure component Aframe

---
+++ src/play-pause-video-component.js
@@ -0,0 +1,34 @@
+AFRAME.registerComponent('play-pause-video', {
+    playBool:true,
+    schema: {
+      target: {type: 'selector', default:'#videosphere'}
+    },
+    init: function () {
+      this.playBool = true
+      console.log("init")
+      this.clickEvent = this.clickEvent.bind(this)
+      this.el.addEventListener('click', this.clickEvent);
+      
+    },
+    clickEvent:function (event) {
+        console.log("clicked",event)
+        var video = this.data.target.components.material.material.map.image;
+        if(this.playBool == false)return;
+        if (video.paused) {
+          video.play();
+        } else {
+          video.pause();
+        }
+    },
+    remove: function () {
+      this.el.removeEventListener('click', this.clickEvent);
+    },
+    play: function () {
+      this.playBool = true;
+    },
+    pause: function () {
+      this.playBool = false;
+      var video = this.data.target.components.material.material.map.image;
+      video.pause();
+    }
+  });
